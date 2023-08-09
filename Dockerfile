03522cedcc56923a60ef0889d9e84e91cce6abaa
Joseph Giovanelli <41596745+josephgiovanelli@users.noreply.github.com>
2023-08-09 15:21:17 +0200

    Create log folder

--- Dockerfile
+++ Dockerfile
@@ -10,10 +10,6 @@ COPY requirements.txt requirements.txt
 RUN pip install --no-cache-dir --upgrade -r requirements.txt && \
     rm requirements.txt
 
-RUN wget -c https://github.com/slds-lmu/yahpo_data/archive/refs/tags/v1.0.zip && \
-    unzip v1.0.zip && \
-    rm -rf v1.0.zip
-
-RUN cd home && mkdir interactive-mo-ml
+RUN mkdir interactive-mo-ml
 WORKDIR /home/interactive-mo-ml
 ENTRYPOINT ["./scripts/wrapper_experiments.sh"]
--- src/experiments_launcher.py
+++ src/experiments_launcher.py
@@ -12,6 +12,22 @@ from utils.common import get_tuning_datasets, make_dir
 
 if __name__ == "__main__":
     input_path = make_dir(os.path.join("/", "home", "input"))
+    common_log_path = make_dir(os.path.join("/", "home", "logs"))
+    log_paths = {
+        "preliminar_sampling": make_dir(
+            os.path.join(common_log_path, "preliminar_sampling")
+        ),
+        "automatic_ordering": make_dir(
+            os.path.join(common_log_path, "automatic_ordering")
+        ),
+        "preference_learning": make_dir(
+            os.path.join(common_log_path, "preference_learning_eval")
+        ),
+        "optimization": make_dir(os.path.join(common_log_path, "optimization")),
+        "comparison": make_dir(os.path.join(common_log_path, "comparison")),
+        "summarizer": make_dir(os.path.join(common_log_path, "summarizer")),
+        "plotter": make_dir(os.path.join(common_log_path, "plotter")),
+    }
 
     print("--- SCENARIO GENERATION ---")
     subprocess.call("python src/scenario_generator.py", shell=True)
@@ -20,24 +36,40 @@ if __name__ == "__main__":
     print("--- PRELIMINAR SAMPLING ---")
     with tqdm(total=len(confs)) as pbar:
         for conf in confs:
+            log_file_name = conf.split(".")[0]
             subprocess.call(
-                f"python src/preliminar_sampling.py --conf_file {conf}", shell=True
+                f"""python src/preliminar_sampling.py --conf_file {conf} > {log_paths["preliminar_sampling"]}/{log_file_name}_out.txt""",
+                shell=True,
             )
             subprocess.call(
-                f"python src/automatic_ordering.py --conf_file {conf}", shell=True
+                f"""python src/automatic_ordering.py --conf_file {conf} > {log_paths["automatic_ordering"]}/{log_file_name}_out.txt""",
+                shell=True,
             )
             pbar.update()
     print("--- PREFERENCE LEARNING ---")
-    subprocess.call(f"python src/preference_learning_eval.py", shell=True)
+    subprocess.call(
+        f"""python src/preference_learning_eval.py > {log_paths["preference_learning"]}/out.txt""",
+        shell=True,
+    )
 
     evaluation_confs = [elem for elem in confs if elem not in get_tuning_datasets()]
     print("--- OPTIMIZATION LOOP ---")
     with tqdm(total=len(evaluation_confs)) as pbar:
         for conf in evaluation_confs:
             subprocess.call(
-                f"python src/optimization.py --conf_file {conf}", shell=True
+                f"""python src/optimization.py --conf_file {conf} > {log_paths["optimization"]}/{log_file_name}_out.txt""",
+                shell=True,
             )
-            subprocess.call(f"python src/comparison.py --conf_file {conf}", shell=True)
+            subprocess.call(
+                f"""python src/comparison.py --conf_file {conf} > {log_paths["comparison"]}/{log_file_name}_out.txt""",
+                shell=True,
+            )
 
-    subprocess.call(f"python src/summarizer.py", shell=True)
-    subprocess.call(f"python src/plotter.py", shell=True)
+    subprocess.call(
+        f"""python src/summarizer.py > {log_paths["summarizer"]}/{log_file_name}_out.txt""",
+        shell=True,
+    )
+    subprocess.call(
+        f"""python src/plotter.py > {log_paths["plotter"]}/{log_file_name}_out.txt""",
+        shell=True,
+    )
